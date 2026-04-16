531eaff765e8cbe11bc4a976d4b25915380e6a53
max-pn <maximilian.partin@icloud.com>
2026-04-16 15:36:00 +0200

    fixed pylint venv issue

--- lua/max-pn/plugins/editor/linting.lua
+++ lua/max-pn/plugins/editor/linting.lua
@@ -1,30 +1,35 @@
 return {
-  "mfussenegger/nvim-lint",
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    local lint = require("lint")
+	"mfussenegger/nvim-lint",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local lint = require("lint")
 
-    lint.linters_by_ft = {
-      java = { "checkstyle" },
-      javascript = { "biomejs", "eslint_d" },
-      javascriptreact = { "biomejs", "eslint_d" },
-      python = { "pylint" },
-      svelte = { "eslint_d" },
-      typescript = { "biomejs", "eslint_d" },
-      typescriptreact = { "biomejs", "eslint_d" },
-    }
+		lint.linters_by_ft = {
+			java = { "checkstyle" },
+			javascript = { "biomejs", "eslint_d" },
+			javascriptreact = { "biomejs", "eslint_d" },
+			python = { "pylint" },
+			svelte = { "eslint_d" },
+			typescript = { "biomejs", "eslint_d" },
+			typescriptreact = { "biomejs", "eslint_d" },
+		}
 
-    local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+		-- fix pylint venv issue
+		if vim.env.VIRTUAL_ENV then
+			lint.linters.pylint.cmd = vim.env.VIRTUAL_ENV .. "/bin/pylint"
+		end
 
-    vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-      group = lint_augroup,
-      callback = function()
-        lint.try_lint()
-      end,
-    })
+		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
-    vim.keymap.set("n", "<leader>l", function()
-      lint.try_lint()
-    end, { desc = "Trigger linting for current file" })
-  end,
+		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+			group = lint_augroup,
+			callback = function()
+				lint.try_lint()
+			end,
+		})
+
+		vim.keymap.set("n", "<leader>l", function()
+			lint.try_lint()
+		end, { desc = "Trigger linting for current file" })
+	end,
 }

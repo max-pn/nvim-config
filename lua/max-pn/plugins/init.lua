40b946887bfe0a3c8ff35fb003f73b4a2ecebea1
max-pn <maximilian.partin@icloud.com>
2026-07-09 17:49:04 +0200

    cleanup

--- lua/max-pn/plugins/init.lua
+++ lua/max-pn/plugins/init.lua
@@ -3,7 +3,6 @@
 --- @date 2025-12-06
 
 return {
-  "nvim-lua/plenary.nvim",          -- lua functions that many plugins use
-  "christoomey/vim-tmux-navigator", -- tmux & split window navigation
-  "neovim/nvim-lspconfig"
+	"nvim-lua/plenary.nvim", -- lua functions that many plugins use
+	"christoomey/vim-tmux-navigator", -- tmux & split window navigation
 }
